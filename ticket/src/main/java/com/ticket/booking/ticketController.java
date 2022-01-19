e8f224c0c92beacdbb9b3518d9907e9e830289e0
srivatsa <srivatsa.a.malali@gmail.com>
2022-01-19 15:48:50 +0530

    put and insert of ticket

--- ticket/src/main/java/com/ticket/booking/ticketController.java
+++ ticket/src/main/java/com/ticket/booking/ticketController.java
@@ -1,10 +1,14 @@
 package com.ticket.booking;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +19,21 @@ public class ticketController {
 	@Autowired
 	ticketService tService;
 	
-	@GetMapping("/getAllTickets")
+	@GetMapping("/get")
 	public List<ticket> geTickets(){
 		List<ticket> tkList=tService.geTickets();
 		return tkList;
+	}
+	
+	@PostMapping("/add")
+	public int saveTicket(@RequestBody ticket Ticket) {
+		tService.saveTicket(Ticket);
+		return 1;
+	}
+	
+	@PutMapping("/{id}")
+	public Optional<ticket> findById(@PathVariable("id") int ticket_number) {
+		return tService.finfById(ticket_number);
 	}
 
 }
--- ticket/src/main/java/com/ticket/booking/ticketService.java
+++ ticket/src/main/java/com/ticket/booking/ticketService.java
@@ -1,6 +1,7 @@
 package com.ticket.booking;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,13 @@ public class ticketService {
 	public List<ticket> geTickets(){
 		List<ticket> tkList=tRepo.findAll();
 		return tkList;
+	}
+	
+	public void saveTicket(ticket Ticket) {
+		tRepo.save(Ticket);
+	}
+	
+	public Optional<ticket> finfById(int ticket_number) {
+		return tRepo.findById(ticket_number);
 	}
 }
